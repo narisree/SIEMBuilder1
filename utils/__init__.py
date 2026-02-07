b1957907d9a3f57a21c386a7fda308e8c30294de
narisree <156998393+narisree@users.noreply.github.com>
2026-02-07 14:18:00 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,4 +6,19 @@ from .kb_loader import KBLoader
 from .ai_client import AIClientFactory, ClaudeClient, GroqClient, HuggingFaceClient, OllamaClient
 from .usecase_loader import UseCaseLoader
 
-__all__ = ['KBLoader', 'AIClientFactory', 'ClaudeClient', 'GroqClient', 'HuggingFaceClient', 'OllamaClient', 'UseCaseLoader']
+# Detection engine is optional (requires pysigma)
+try:
+    from .detection_engine import DetectionEngine
+except ImportError:
+    DetectionEngine = None
+
+__all__ = [
+    'KBLoader',
+    'AIClientFactory',
+    'ClaudeClient',
+    'GroqClient',
+    'HuggingFaceClient',
+    'OllamaClient',
+    'UseCaseLoader',
+    'DetectionEngine',
+]
